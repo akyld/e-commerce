9e532dceaa380ba2aeb2ba3212edaa8d6b3be1b4
emrhanaksu <emrhanaksuu@gmail.com>
2024-05-24 16:36:00 +0300

    commit

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,29 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const dotenv = require('dotenv')
-const app = express()
-const mainRoute = require('./routes/index.js')
-const port = 5000
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const app = express();
+const logger = require("morgan");
+const mainRoute = require("./routes/index.js");
+const port = 5000;
 
-dotenv.config()
+dotenv.config();
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log('Connected to MongoDB')
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to MongoDB");
   } catch (error) {
-    console.log('Error connecting to MongoDB', error)
+    console.log("Error connecting to MongoDB", error);
   }
-}
+};
 
-app.use('/api', mainRoute)
+// middlewares
+app.use(logger("dev"));
+app.use(express.json());
+
+app.use("/api", mainRoute);
 
 app.listen(port, () => {
-  connect()
-  console.log(`Server is running at ${port}`)
-})
+  connect();
+  console.log(`Server is running at ${port}`);
+});
