372ce97b00fa5734d22e1305ac5a24e9df586b5a
akyld <ak.yldrm1242@gmail.com>
2024-05-29 02:42:50 +0300

    final commit

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,31 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const app = express();
-const cors = require("cors");
-const logger = require("morgan");
-const mainRoute = require("./routes/index.js");
-const port = 5000;
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const app = express()
+const cors = require('cors')
+const logger = require('morgan')
+const mainRoute = require('./routes/index.js')
+const port = 5000
 
-dotenv.config();
+dotenv.config()
 
 const connect = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("Connected to mongoDb");
+    await mongoose.connect(process.env.MONGO_URI)
+    console.log('Connected to mongoDb')
   } catch (error) {
-    throw error;
+    throw error
   }
-};
+}
 
 // middlewares
-app.use(logger("dev"));
-app.use(express.json());
-app.use(cors());
+app.use(logger('dev'))
+app.use(express.json())
+app.use(cors())
 
-app.use("/api", mainRoute);
+app.use('/api', mainRoute)
 
 app.listen(port, () => {
-  connect();
-  console.log(`Sunucu ${port} portunda çalışıyor.`);
-});
+  connect()
+  console.log(`Sunucu ${port} portunda çalışıyor.`)
+})
